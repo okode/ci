c941596aceaa2a72690f427ae37b11e2928ce657
Pedro Jorquera <pjorquera@okode.com>
2019-01-21 18:18:49 +0100

    Updated generated code

--- dist/ci.js
+++ dist/ci.js
@@ -79,6 +79,8 @@ class CICommand {
                     {
                         utils_1.Utils.exec('sudo npm install --quiet -g ionic');
                         if (nativeBuild) {
+                            utils_1.Utils.exec('yes | sdkmanager --licenses');
+                            utils_1.Utils.exec('sdkmanager "build-tools;28.0.3" "platforms;android-28" "platform-tools" "tools"');
                             utils_1.Utils.exec('sudo npm install --quiet -g cordova');
                             utils_1.Utils.exec('cordova telemetry off');
                             const gradleVersion = '5.0';
